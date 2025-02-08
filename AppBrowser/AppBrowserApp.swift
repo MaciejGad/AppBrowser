fcfa7d87d06c002df845624462969c003cd76fb9
Maciej Gad <gad.maciej@gmail.com>
2025-02-08 18:13:22 +0100

    Initial Commit

---
+++ AppBrowser/AppBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBrowserApp.swift
+//  AppBrowser
+//
+//  Created by Maciej Gad on 08/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
