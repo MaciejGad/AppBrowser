0c0ccff8b1c164e477a68925aa2a170e073d04b6
Maciej Gad <gad.maciej@gmail.com>
2025-03-03 23:36:24 +0100

    move BrowserToolbar to ToolbarContent

--- AppBrowser/Browser/BrowserToolbar.swift
+++ AppBrowser/Browser/BrowserToolbar.swift
@@ -1,35 +1,29 @@
 import SwiftUI
 
-struct BrowserToolbar: View {
+struct BrowserToolbar: ToolbarContent {
     @EnvironmentObject private var viewModel: BrowserViewModel
     @EnvironmentObject private var safe: SafeBrowsingViewModel
 
-    var body: some View {
-        VStack(spacing: 5) {
-            if viewModel.showPath {
-                Text(viewModel.curentPath)
-                    .multilineTextAlignment(.trailing)
-                    .frame(minWidth: 100, maxWidth: .infinity)
-            }
-            HStack(spacing: 10) {
-                let lastElement = viewModel.toolbarItems.last
-                ForEach(viewModel.toolbarItems) { item in
-                    Button(action: {
-                        viewModel.handle(command: item)
-                    }) {
-                        Image(systemName: item.iconName())
-                            .font(.system(size: 24))
-                    }.disabled(
-                        item == .goBack && !viewModel.canGoBack
-                    )
-                    if item != lastElement {
-                        Spacer()
+    var body: some ToolbarContent {
+        ToolbarItem(placement: .bottomBar) {
+            VStack(spacing: 5) {
+                HStack(spacing: 10) {
+                    let lastElement = viewModel.toolbarItems.last
+                    ForEach(viewModel.toolbarItems) { item in
+                        Button(action: {
+                            viewModel.handle(command: item)
+                        }) {
+                            Image(systemName: item.iconName())
+                                .font(.system(size: 20))
+                        }.disabled(
+                            item == .goBack && !viewModel.canGoBack
+                        )
+                        if item != lastElement {
+                            Spacer()
+                        }
                     }
                 }
-            }.padding(.horizontal, 30)
+            }
         }
-        .padding(.top, 10)
-        .padding(.bottom, 15)
-        .background(Color(UIColor.systemGray6))
     }
 }
--- AppBrowser/Browser/BrowserView.swift
+++ AppBrowser/Browser/BrowserView.swift
@@ -5,23 +5,31 @@ struct BrowserView: View {
     @EnvironmentObject private var safeBrowsing: SafeBrowsingViewModel
     
     var body: some View {
-        VStack(spacing: 0) {
-            ZStack {
-                WebView()
-                    .edgesIgnoringSafeArea(.all)
-                if let error = viewModel.error {
-                    ErrorView(
-                        title: "Browser Error",
-                        message: error.localizedDescription
-                    )
-                    Spacer()
-                }
-                if viewModel.isLoading {
-                    ProgressView()
+        NavigationView {
+            VStack(spacing: 0) {
+                ZStack {
+                    WebView()
+                        .edgesIgnoringSafeArea(.all)
+                    if let error = viewModel.error {
+                        ErrorView(
+                            title: "Browser Error",
+                            message: error.localizedDescription
+                        )
+                        Spacer()
+                    }
+                    if viewModel.isLoading {
+                        ProgressView()
+                    }
                 }
             }
+            .navigationTitle(viewModel.curentPath)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(!viewModel.showPath)
+        }
+        .toolbar {
             BrowserToolbar()
         }
+       
         .alert(item: $safeBrowsing.alertData) { alertData in
             Alert(
                 title: Text(alertData.title),
